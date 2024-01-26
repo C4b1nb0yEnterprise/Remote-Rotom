9cd6a415e71764a4065e88fea18c633177771581
C4b1nb0yEnterprise <admin@pogomap.koeln>
2024-01-26 22:17:45 +0100

    Working messages; still needs some cleanup and the actual job request

--- commands/rotom/run-job.js
+++ commands/rotom/run-job.js
@@ -1,10 +1,10 @@
-const { EmbedBuilder, SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ComponentType, time } = require('discord.js');
+const { EmbedBuilder, SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ComponentType, time, hyperlink } = require('discord.js');
 const { rotom } = require('../../config.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('run-job')
-		.setDescription(`Select a device to run a job on.`),
+		.setDescription('Select a device to run a job on.'),
 	async execute(interaction) {
 		console.log(`User ${interaction.user.username} requested a job execution.`);
 
@@ -52,19 +52,10 @@ module.exports = {
 		}
 
 
-		const cancel = new ButtonBuilder()
-			.setCustomId('cancel')
-			.setLabel('Cancel')
-			.setStyle(ButtonStyle.Secondary);
-
-		const confirmJob = new ButtonBuilder()
-			.setCustomId('yes')
-			.setLabel('Yes')
-			.setStyle(ButtonStyle.Danger);
-
-		const jobSelect = new StringSelectMenuBuilder()
-			.setCustomId('job-to-run')
-			.setPlaceholder('Select a job')
+		const selectJob = new StringSelectMenuBuilder()
+			.setCustomId('job')
+			.setPlaceholder('Select a job');
+			
 
 		// Get Job List
 		const responseJobList = await fetch(rotom.address + "/api/job/list");
@@ -76,11 +67,11 @@ module.exports = {
 		for (const job in rotomJobList) {
 			if (rotomJobList.hasOwnProperty(job)) {
 				console.log(`Found Job ${rotomJobList[job].id}`);
-				jobSelect.addOptions(
-				new StringSelectMenuOptionBuilder()
-					.setLabel(rotomJobList[job].id)
-					.setDescription(rotomJobList[job].description)
-					.setValue(rotomJobList[job].id)
+				selectJob.addOptions(
+					new StringSelectMenuOptionBuilder()
+						.setLabel(rotomJobList[job].id)
+						.setDescription(rotomJobList[job].description)
+						.setValue(rotomJobList[job].id)
 				);
 				jobCounter++
 				jobIdList.push(rotomJobList[job].id);
@@ -90,8 +81,11 @@ module.exports = {
 			}
 		}
 
-		const deviceSelect = new StringSelectMenuBuilder()
-			.setCustomId('device-to-execute')
+		const jobSelect = new ActionRowBuilder()
+			.addComponents(selectJob);
+
+		const selectDevice = new StringSelectMenuBuilder()
+			.setCustomId('device')
 			.setPlaceholder('Select a device')
 			.addOptions(
 				new StringSelectMenuOptionBuilder()
@@ -107,7 +101,7 @@ module.exports = {
 				deviceOnlineStatus = "Online";
 			}
 
-			deviceSelect.addOptions(
+			selectDevice.addOptions(
 				new StringSelectMenuOptionBuilder()
 					.setLabel(rotomStatus.devices[dev].origin)
 					.setDescription(deviceOnlineStatus)
@@ -116,132 +110,65 @@ module.exports = {
 
 		}
 
-		const confirmJobExecution = new ActionRowBuilder()
-			.addComponents( confirmJob, cancel );
+		const deviceSelect = new ActionRowBuilder()
+			.addComponents(selectDevice);
 
-		const deviceSelectionMenu = new ActionRowBuilder()
-			.addComponents( deviceSelect );
 
-		const jobSelectionMenu = new ActionRowBuilder()
-			.addComponents( jobSelect );
+		const cancel = new ButtonBuilder()
+			.setCustomId('cancel')
+			.setLabel('Cancel')
+			.setStyle(ButtonStyle.Secondary);
 
-		// Get Job selection
-		const responseJobInteraction = await interaction.reply({ 
-			content: `**Please select Job and Device to run?**\nOnly shows first 25 jobs of your instance.\nDevices online: ${deviceOnlineCounter}/${rotomStatus.devices.length}`, 
-			components: [ jobSelectionMenu, deviceSelectionMenu ],
-			embeds: deviceEmbeds, 
+		const restartAllYes = new ButtonBuilder()
+			.setCustomId('yes')
+			.setLabel('Yes')
+			.setStyle(ButtonStyle.Danger);
+
+		const confirmRestart = new ActionRowBuilder()
+			.addComponents( restartAllYes, cancel );
+
+		//await interaction.deferReply()
+		//await interaction.deferReply();
+		const responseJobSelect = await interaction.reply({
+			content: `**Please select Job to run.**\nOnly shows first 25 jobs of your instance.`, 
+			components: [ jobSelect ],
 			ephemeral: true 
 		});
 
-		const collectorFilter = i => i.user.id === interaction.user.id;
+		const collectorFilterJob = i => i.user.id === interaction.user.id && i.customId === 'job';
+		const collectorFilterDevice = i => i.user.id === interaction.user.id && i.customId === 'device';
+		const collectorFilterConfirm = i => i.user.id === interaction.user.id && i.customId === 'yes' || i.user.id === interaction.user.id && i.customId === 'cancel';
 
-		// Get User Device Selection
-		const collectorJob = responseJobInteraction.createMessageComponentCollector({ filter: collectorFilter, componentType: ComponentType.StringSelect, time: 3_600_000 });
+		const collectorJobSelect = responseJobSelect.createMessageComponentCollector({ componentType: ComponentType.StringSelect, filter: collectorFilterJob, time: 3_600_000 });
+		const collectorDeviceSelect = responseJobSelect.createMessageComponentCollector({ componentType: ComponentType.StringSelect, filter: collectorFilterDevice, time: 3_600_000 });
 
-		collectorJob.on('collect', async j => {
-			const selectedJob = j.values[0];
-			const selectedDevice = j.values[1];
-			console.log(`${j.user.username} has selected ${selectedJob} on ${selectedDevice} !`);
+		let selectionJob = "";
+		let selectionDevice = "";
 
-			// if (jobIdList.includes(selection)){
-			// 	console.log("Job was selected!");
-			// 	let selectedJob = selection;
+		collectorJobSelect.on('collect', async i => {
+			selectionJob = i.values[0];
+			console.log(`${i.user} has selected Job ${selectionJob}!`);
+			await i.update({content: `Please select Device:`, components: [deviceSelect], embeds: deviceEmbeds, ephemeral: true });
+		});
 
-			// 	await interaction.editReply({ 
-			// 		content: `**On which Device do you want to run the job?**\nDevices online: ${deviceOnlineCounter}/${rotomStatus.devices.length}`, 
-			// 		components: [ deviceSelectionMenu ], 
-			// 		embeds: deviceEmbeds, 
-			// 		ephemeral: true 
-			// 	});
-			// }
+		collectorDeviceSelect.on('collect', async i => {
+			selectionDevice = i.values[0];
+			console.log(`${i.user} has selected Device ${selectionDevice}!`);
+			const userConfirmation = await i.update({ content: `⚠️ Are you sure, you want to run **${selectionJob}** on **${selectionDevice}**?`, embeds: [], components: [ confirmRestart ] });
 
-			if (selectedDevice === "all" || rotomStatus.devices.find(item => item.deviceId === selectedDevice)){
-				let selectionLabel = "all devices";
+			const restartUserConfirmation = await userConfirmation.awaitMessageComponent({ filter: collectorFilterConfirm, time: 180_000 });
+			if (restartUserConfirmation.customId === "yes"){
+				console.log("He said yes!");
+				await i.editReply({ content: `✅ Successfully ran **${selectionJob}** on **${selectionDevice}**!`, embeds: [], components: [], ephemeral: true })
 
-				if (selectedDevice != "all"){
-					let selectedDevice = rotomStatus.devices.find(item => item.deviceId === selectedDevice);
-					selectionLabel = selectedDevice.origin;
-				}
-
-				////////////////////////////////////////////////
-				/////// Seems to work up until here... WIP /////
-				////////////////////////////////////////////////
-
-				// Confirmation Flow
-				const userConfirmation = await j.editReply({ content: `⚠️ Are you sure, you want to run **${selectedJob}** on **${selectionLabel}**?`, embeds: [], components: [ confirmJobExecution ] });
-
-				// Confirm Job
-				try {
-					const userConfirmation = await userConfirmation.awaitMessageComponent({ filter: collectorFilter, time: 180_000 });
-
-					if (userConfirmation.customId === 'yes') {
-						console.log(`${userConfirmation.user.username} confirmed!`)
-						await userConfirmation.deferReply({ ephemeral: true });
-						await interaction.deleteReply();
-						//await userConfirmation.deleteReply();
-
-						//restart device here
-						if (selectedDevice === "all"){
-							console.log("Looping all devices.");
-							for (let dev = 0; dev < rotomStatus.devices.length; dev++) {
-								console.log(`Run Job on ${rotomStatus.devices[dev].deviceId} now!`);
-
-								try {
-								    let response = await fetch(rotom.address + '/api/job/execute/' + selectedJob, {
-									    method: "POST",
-									    body: JSON.stringify({deviceIdsOrOrigins: [ rotomStatus.devices[dev].deviceId ]})
-									});
-								    if (!response.ok) {
-								      	throw new Error("Network response was not OK");
-								      }
-								  } catch (error) {
-								    console.error("There has been a problem with your fetch operation:", error);
-								}							
-							}
-						} else {
-							console.log(`Run Job on ${selectedDevice} now!`);
-
-							try {
-							    let response = await fetch(rotom.address + '/api/job/execute/' + jobSelected, {
-								    method: "POST",
-								    body: JSON.stringify({deviceIdsOrOrigins: [ selectedDevice ]})
-								});
-							    if (!response.ok) {
-							      	throw new Error("Network response was not OK");
-							     }
-							  } catch (error) {
-							    console.error("There has been a problem with your fetch operation:", error);
-							}							
-						}
-						console.log("Done executing jobs! ");
-						await userConfirmation.editReply({ content: `✅ Successfully ran **${selectedJob}** on **${selectionLabel}**!`, embeds: [], components: [], ephemeral: true })
-
-					} else if (userConfirmation.customId === 'cancel') {
-						await userConfirmation.update({ content: 'Action cancelled', embeds: [], components: [] });
-					}
-
-				} catch (e) {
-					await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', embeds: [], components: [] });
-				}
+			} else if (restartUserConfirmation.customId === "cancel"){
+				console.log("He said no...");
+				await i.editReply({ content: 'Job Execution cancelled', embeds: [], components: [] });
 			}
 
-			// Get Device Selection
-			// const responseInteraction = await interaction.editReply({ 
-			// 	content: `**On which Device do you want to run the job?**\nDevices online: ${deviceOnlineCounter}/${rotomStatus.devices.length}`, 
-			// 	components: [ deviceSelectionMenu ], 
-			// 	embeds: deviceEmbeds, 
-			// 	ephemeral: true 
-			// });
+		});
 
-			// Get User Device Selection
-			// const collector = responseInteraction.createMessageComponentCollector({ filter: collectorFilter, componentType: ComponentType.StringSelect, time: 3_600_000 });
 
-			// collector.on('collect', async i => {
-			// 	const selection = i.values[0];
-			// 	console.log(`${i.user.username} has selected Device ${selection}!`);
 
-				
-			// });	
-		});		
-	}
+	},
 };
